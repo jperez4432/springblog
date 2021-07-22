42be0ed1aa5ebef27f9a4962cede1010e06d83b1
Juan Perez <jperez4432@gmail.com>
2021-07-22 17:02:03 -0500

    eod, troubleshooting to see why server wont run

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,35 +1,45 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
+    private List<Post> posts = new ArrayList<>();
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String Posts() {
-        return "<h1>Posts index page</h1>";
+    public String viewPosts(Model model) {
+        posts.add(new Post("This is post1", "This is post1s body"));
+        posts.add(new Post("This is post2", "This is post2s body"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
-    @GetMapping("/posts{id}")
-    @ResponseBody
-    public String PostsByID(@PathVariable int id) {
-        return "<h1>View individual post " + id + "</h1>";
+    @GetMapping("/posts/{id}")
+    public String singlePost(@PathVariable long id, Model model) {
+        Post post = new Post("Jeff buys bicycle.", "No one know why. Must really like the feeling of the wind on his face.");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
+    // When you visit the URL you will see the form to create a post.
     @GetMapping("/posts/create")
     @ResponseBody
-    public String CreatePost() {
-        return "<h1>View the form for creating a post</h1>";
+    public String createForm() {
+        return "View form to create a post.";
     }
 
+    // When you submit the form on the /posts/create page,
+    // the information will be posted to the same URL
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     @PostMapping("/posts/create")
     @ResponseBody
-    public String hello() {
-        return "<h1>Create a new post</h1>";
+    public String createPost() {
+        return "Creates new post.";
     }
 }
--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,14 +1,24 @@
-//package com.codeup.springblog.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class RollDiceController {
-//    @GetMapping("/roll-dice")
-//    public String rollDice(Int Dice) {
-//
-//        int outcome = (int)(Math.random()*6) + 1;
-//
-//    }
-//}
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String guess() {
+        return "guess";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String checkGuess(@PathVariable int n, Model model) {
+        int outcome = (int) Math.floor((Math.random() * 6) + 1);
+        boolean check = outcome == n;
+        model.addAttribute("n", n);
+        model.addAttribute("outcome", outcome);
+        model.addAttribute("check", check);
+        return "dice-roll-check";
+    }
+}
