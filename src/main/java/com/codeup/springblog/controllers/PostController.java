579f7c10bf4350a6832e4798b2837e49819f85ef
Juan Perez <jperez4432@gmail.com>
2021-07-25 23:06:57 -0500

    edited ads/index/show files. will continue to edit to add bootstrap and better desing. functionality is working

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     private final PostRepository postDao;
@@ -20,17 +23,6 @@ public class PostController {
         return "posts/index";
     }
 
-    @GetMapping("/join")
-    public String showJoinForm() {
-        return "join";
-    }
-
-    @PostMapping("/join")
-    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
-        model.addAttribute("cohort", "Welcome to " + cohort + "!");
-        return "join";
-    }
-
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
         model.addAttribute("post", postDao.getById(id));
@@ -43,20 +35,20 @@ public class PostController {
         return "posts/edit";
     }
 
-//    @PostMapping("/posts/edit/{id}")
-//    public String editPost(@PathVariable long id, Model model) {
-//        model.addAttribute()
-//                return "redirect:/posts/";
-//    }
-
-
-
-    @PostMapping("/posts/delete/{id}")
-    public String deletePost(@PathVariable long id, Model model) {
-        postDao.deleteById(id);
-        return "redirect:/posts/";
+    @PostMapping("/posts/edit/{id}")
+    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+        Post post = postDao.getById(id);
+        post.setTitle(title);
+        post.setBody(body);
+        postDao.save(post);
+        return "redirect:/posts/" + id;
     }
 
+    @PostMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        postDao.delete(postDao.getById(id));
+        return "redirect:/posts";
+    }
 
     // When you visit the URL you will see the form to create a post.
     @GetMapping("/posts/create")
