3c3236e16d0bf1f97bbf6e210fc34a42dd03c7c7
Juan Perez <jperez4432@gmail.com>
2021-08-07 03:19:41 -0500

    have all files linked. working on forms so they are equal sizes

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/posts/index")
     public String showLanding() {
-        return "<h1>This is the landing page!</h1>";
+        return "/posts/index";
     }
 }
--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -17,13 +17,13 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/sign-up")
+    @GetMapping("/register")
     public String showSignupForm(Model model) {
         model.addAttribute("user", new User());
         return "users/register";
     }
 
-    @PostMapping("/sign-up")
+    @PostMapping("/register")
     public String saveUser(@ModelAttribute User user) {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
